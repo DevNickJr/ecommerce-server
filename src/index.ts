2f1c33b4eaeb9332efd20c03a725e0abf4753d1a
DevNickjr <nicholasduadei14@gmail.com>
2024-02-07 19:30:08 +0100

    connect and sync db

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
 import express, { Express, Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import { connect } from "./utils/db" 
+import { syncDB } from "./models";
 
 dotenv.config();
 
-
 const app: Application = express();
 const port = process.env.PORT || 3000;
-
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+const main = async () => {
+  try {
+    await connect()
+    await syncDB()
+    app.listen(port, () => {
+      console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
+    
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+main()
+
---
+++ src/utils/db.ts
@@ -0,0 +1,40 @@
+import { Dialect, Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const username = process.env.DB_USERNAME || '';
+const password = process.env.DB_PASSWORD || "";
+const host = process.env.DB_HOST || "";
+const dialect: Dialect = process.env.DB_DIALECT as Dialect || "mysql";
+
+// console.log({
+//   username,
+//   password,
+//   host,
+//   dialect
+// })
+
+const sequelize = new Sequelize(
+    'hello_world_db',
+    username,
+    password,
+     {
+       host,
+       dialect,
+     }
+);
+
+
+const connect = async () => {
+  if (!username || !password || !host || !dialect) {
+    throw new Error("Provide all Database Environment Variables")
+  }
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+}
+
+export {
+    connect,
+    sequelize
+}
