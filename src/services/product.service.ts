04593879e5cd5a3f674d5ebd28097e7d2338998b
DevNickjr <nicholasduadei14@gmail.com>
2024-02-28 14:59:05 +0100

    restructure complete

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,20 +1,17 @@
+import { CreationAttributes, Op } from "sequelize";
 import { MakeNullishOptional } from "sequelize/types/utils";
 import { Category, Product } from "../models"
 
 import CRUD from "./crud"
-import { Op } from "sequelize";
 
 
 class ProductService extends CRUD<Product> {
-    async createProduct(fields: MakeNullishOptional<Product["_creationAttributes"]>, categories: string[]): Promise<Product> {
+    async createProduct(fields: CreationAttributes<Product>, categories: string[]): Promise<Product> {
         const data = await this.model.create(fields);
 
         const catgs = await Category.findAll({ where: { title: {
             [Op.in]: categories
         }}});
-
-        // await data.addCategories(catgs);
-        // const data = await this.model.create(fields);
         return data;
     }
 }
